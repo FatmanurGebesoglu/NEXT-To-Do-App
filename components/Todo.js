f48e40f965b5a2b2f1ec3f3fbfa99f1219e4e763
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-19 17:23:19 +0300

    Todo Context Created

--- components/Todo.js
+++ components/Todo.js
@@ -1,12 +1,12 @@
-import { ListItem, ListItemText } from "@mui/material"
+import { colors, ListItem, ListItemText } from "@mui/material"
 import moment from "moment/moment"
 import 'moment/locale/tr';
 
 export default function Todo({todo}) {
     const {id, baslik,aciklama,tarih} = todo;
   return (
-    <ListItem sx={{mt:3, boxShadow:3}} style={{backgroundColor:'#FAFAFA'}}>
-        <ListItemText primary={baslik} secondary={moment(tarih).format('LLL')} />
+    <ListItem  sx={{mt:3, boxShadow:3}} style={{backgroundColor:'#FAFAFA', color: 'black'}}>
+        <ListItemText  primary={baslik} secondary={moment(tarih).format('LLL')} />
          
     </ListItem>
 
